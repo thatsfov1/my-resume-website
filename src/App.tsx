dcc656b7f68ba3beb562c0a7811a7821023d2e8c
thatsfov1 <zheniaukr@icloud.com>
2023-12-15 19:08:13 +0100

    added bird, plane, sky, island

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Contacts from "./pages/Contacts";
+import About from "./pages/About";
 
 function App() {
   return (
-    <>
-
-    </>
+    <main className='bg-slate-300/20'>
+        <BrowserRouter>
+            <Navbar/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/about' element={<About/>}/>
+                <Route path='/contacts' element={<Contacts/>}/>
+                <Route path='/projects' element={<Projects/>}/>
+            </Routes>
+        </BrowserRouter>
+    </main>
   )
 }
 
---
+++ src/models/Plane.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {useGLTF} from "@react-three/drei";
+import planeScene from "../assets/3d/plane.glb";
+
+
+const Plane = () => {
+  const {scene, animation} = useGLTF(planeScene)
+  return (
+    <mesh>
+      <primitive object={scene}/>
+    </mesh>
+  )
+}
+
+export default Plane
